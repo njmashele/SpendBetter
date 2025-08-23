65ac61dd95d31dbd35270f77124f9b25bd190ed4
Jon Mashele <nsovomsh23@gmail.com>
2025-08-23 21:23:03 +0200

    Add project files.

---
+++ SpendBetter/Models/Expense.cs
@@ -0,0 +1,13 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace SpendBetter.Models
+{
+    public class Expense
+    {
+        public int id {  get; set; }
+        public int Value { get; set; }
+
+        [Required]
+        public string? Description { get; set; }
+    }
+}
